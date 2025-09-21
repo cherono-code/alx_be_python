c5589b6a1df35a45164d5cb4610e1b1114fd9be1
“cherono-code” <cheronodaisy38@gmail.com>
2025-09-21 14:21:19 +0300

    fix spelling

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,8 @@
-principle = 1000  # Principal amount    
+principal = 1000  # Principal amount    
 rate = 0.05         # Annual interest rate in decimal
 time = 3         # Time in years
 
 
 # Calculate simple interest
-interest = principle * rate * time
+interest = principal * rate * time
 print(f"The simple interest is: {interest}")
