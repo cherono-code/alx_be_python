347b54c295e565a5dc55b0a982b4f0cbb0dbe94d
“cherono-code” <cheronodaisy38@gmail.com>
2025-09-21 15:52:25 +0300

    future age corrections

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 #ask user for their age
-current_age = int(input(("How old are you? ")))
+age = int(input("How old are you? "))
 
 #calculate age in 2050
 #current_year = 2023
@@ -9,5 +9,5 @@ current_age = int(input(("How old are you? ")))
 years_difference = 27
 
 #finding out future age of 2050
-future_age = current_age + years_difference
+future_age = input + years_difference
 print(f"In 2050, you will be {future_age} years old.")
