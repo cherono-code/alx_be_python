f10c843975f1105fcf2cb4a3cba2f7537c2cb683
“cherono-code” <cheronodaisy38@gmail.com>
2025-09-21 15:47:14 +0300

    debugging

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,5 @@
 #ask user for their age
-age = (int(input(("How old are you? "))))
+current_age = int(input(("How old are you? ")))
 
 #calculate age in 2050
 #current_year = 2023
@@ -9,5 +9,5 @@ age = (int(input(("How old are you? "))))
 years_difference = 27
 
 #finding out future age of 2050
-future_age = age + years_difference
+future_age = current_age + years_difference
 print(f"In 2050, you will be {future_age} years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -3,7 +3,7 @@ hours = 2
 #seconds_in_an_minute = 60
 
 #calculating seconds in an hour
-seconds_in_an_hour = 3600
+#seconds_in_an_hour = 3600
 #converting hours to seconds
-seconds = hours * seconds_in_an_hour
+seconds = hours * 3600
 print(f"{hours} hour(s) is {seconds} seconds")
