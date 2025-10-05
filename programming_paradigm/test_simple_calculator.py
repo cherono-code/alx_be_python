2f601c6ad846babcb7b74073326586fb8a159ddb
“cherono-code” <cheronodaisy38@gmail.com>
2025-10-05 23:36:54 +0300

    debugging

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,10 +6,10 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
 
     def test_multiply(self):
